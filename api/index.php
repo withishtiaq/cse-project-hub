97d5eb96682463125c3fe28c6fec2d39973656ca
Ishtiaq Ahmed <105452019+withishtiaq@users.noreply.github.com>
2026-02-10 16:18:59 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
+// স্ট্যাটিক ফাইল (ইমেজ, সিএসএস) হ্যান্ডেল করার জন্য
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if ($uri !== '/' && file_exists(__DIR__ . '/../public' . $uri)) {
+    return false;
+}
+
 require __DIR__ . '/../public/index.php';
